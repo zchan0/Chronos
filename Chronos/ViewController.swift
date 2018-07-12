a6f6ab47adb061110131dba51d36e895a704e565
Cc Zheng <zchan0@outlook.com>
2018-07-12 14:54:53 +0800

    First commit

---
+++ Chronos/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Chronos
+//
+//  Created by Cencen Zheng on 2018/7/12.
+//  Copyright © 2018 Cencen Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Chronos"
+        view.backgroundColor = UIColor.white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
